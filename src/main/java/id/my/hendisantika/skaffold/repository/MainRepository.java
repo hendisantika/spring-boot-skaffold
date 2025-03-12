90cb354ffbb4c1f4b039e6b2f0ce6692cee67e4d
hendisantika <hendisantika@yahoo.co.id>
2025-03-12 09:37:29 +0700

    feat: Add MainRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/skaffold/repository/MainRepository.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.skaffold.repository;
+
+import id.my.hendisantika.skaffold.domain.Person;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-skaffold
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/03/25
+ * Time: 09.37
+ * To change this template use File | Settings | File Templates.
+ */
+@Repository
+@RequiredArgsConstructor
+public class MainRepository {
+
+    private final FakeDataStore fakeDataStore;
+
+    public List<Person> getFamilyMembers() {
+        return fakeDataStore.getListOfFamilyMembers();
+    }
+}
