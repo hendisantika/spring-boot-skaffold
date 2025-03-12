bee6f08bc36f7b79d0dbb37ad3ff138885814090
hendisantika <hendisantika@yahoo.co.id>
2025-03-12 09:34:20 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/skaffold/SpringBootSkaffoldApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.skaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSkaffoldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSkaffoldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/skaffold/SpringBootSkaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.skaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSkaffoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
